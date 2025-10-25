bce864535835194deca1b055868e36d825f924c5
Ande-glitch <andnguyen24@outlook.com>
2025-10-25 17:36:15 +0200

    first commit

---
+++ readme.md
@@ -0,0 +1,48 @@
+<h1 align="center">Weather API</h1>
+<div align="center">
+  <h3>
+    <a href="https://ande-glitch.github.io/WeatherAPI/index.html">
+      Link to project
+    </a>
+  </h3>
+</div>
+<!-- TABLE OF CONTENTS -->
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Built With](#built-with)
+- [Features](#features)
+- [How to use](#how-to-use)
+- [Contact](#contact)
+
+<!-- OVERVIEW -->
+## Overview
+- En nettside som bruker OMDb API og henter / fetcher data til bruk av anbefalinger.
+
+
+## Hvordan Brukes?
+- På søk feltet, søk på en film, så vil det vises på skjermen.
+
+## Features
+- API call
+- Kvalitets sjekk
+- Algoritmisk sortering av data
+- Dynamisk
+
+## Utfordringer
+- Algoritmisk sortering / kvalitets sjekk av bilder
+- API daglig begrensning på 1000 forespørsel (Viktig) 
+
+## Løsning
+- Samarbeid
+- Hjelp av klassekamerater
+- KI / AI
+
+## Contact
+- GitHub [Ande-Glitch](https://github.com/Ande-glitch)
+- Epost [And Nguyen](mailto:andynuwen@gmail.com)
+
+## Result
+
+![Image_1](./Images/lunch.png)
---
+++ script.js
@@ -0,0 +1,145 @@
+let myKey = `d8d3022d`
+
+let inputBtn = document.getElementById("inputBtn")
+let inputSearch = document.getElementById("inputSearch")
+
+let movieTitle = document.getElementById("movieTitle")
+let movieImg = document.getElementById("movieImg")
+let plot = document.getElementById("plot")
+let country = document.getElementById("country")
+let genre = document.getElementById("genre")
+let released = document.getElementById("released")
+let runtime = document.getElementById("runtime")
+let director = document.getElementById("director")
+let rated = document.getElementById("rated")
+let boxOffice = document.getElementById("boxOffice")
+let year = document.getElementById("year")
+
+let images = document.querySelectorAll(".recom")
+let mov1 = document.getElementById("mov1")
+let mov2 = document.getElementById("mov2")
+let mov3 = document.getElementById("mov3")
+
+let num = null
+let imdbId = null
+let rightCode = "tt0120338"
+
+let recomAPI = `http://www.omdbapi.com/?i=${imdbId}&apikey=${myKey}`
+
+let movieElements = [mov1, mov2, mov3]
+let moviePosters = []
+let validPosters = []
+let movieTitles = []
+
+function checkPoster(url) {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.onload = () => resolve(true);
+    img.onerror = () => resolve(false);
+    img.src = url;
+  });
+}
+
+async function validateMoviePosters() { // venter til at checkPoster function funker
+  for (const url of moviePosters) {
+    const isValid = await checkPoster(url);
+    if (isValid) {
+      validPosters.push(url);
+    } else {
+        console.log("no");
+    }
+  }
+  moviePosters = validPosters;
+}
+
+function refreshRec() {
+
+  if (moviePosters.length >= 3) {
+    for (let i = 0; i < movieElements.length; i++) {
+      movieElements[i].src = moviePosters[i];
+      movieElements[i].alt = movieTitles[i]
+    }
+    return; // stop code
+  }
+
+    document.getElementById("recom-images").style.display = "flex"
+
+    num = Math.floor(Math.random() * 9999999)
+    imdbId = "tt"+num.toString().padStart(7, "0")
+    recomAPI = `http://www.omdbapi.com/?i=${imdbId}&apikey=${myKey}`
+
+    console.log(imdbId);
+
+    fetch(recomAPI)
+    .then(response => response.json())
+    .then(data => {
+        if (data.Response == "False" || data.Poster == "N/A") {
+            refreshRec() //retry
+        }
+        else if (data.Response == "True" && data.Poster != "N/A" && data.Genre){
+            let img = new Image()
+            img.onload = () => {
+                moviePosters.push(data.Poster)
+                movieTitles.push(data.Title)
+                refreshRec()
+            }
+            img.onerror = () => {
+                refreshRec()
+            }
+            img.src = data.Poster
+        }
+    })
+    .catch(err => {
+        refreshRec()
+    })
+
+}
+
+function movie() {
+    for (let i = 0; i < movieElements.length; i++) {
+        movieElements[i].src = ""
+        movieTitles[i] = ""
+    }
+    moviePosters = []
+    movieTitles = []
+    let API = `http://www.omdbapi.com/?t=${inputSearch.value}&apikey=${myKey}`    
+    const randomAPI = `https://www.omdbapi.com/?s=matrix&type=movie&page=2&apikey=${myKey}`
+
+    fetch(API)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById("container2").style.display = "flex"
+        movieTitle.innerHTML = data.Title
+        plot.innerHTML = data.Plot
+        movieImg.src = data.Poster
+        country.innerHTML = data.Country
+        genre.innerHTML = data.Genre
+        released.innerHTML = data.Released
+        runtime.innerHTML = data.Runtime
+        director.innerHTML = data.Director
+        rated.innerHTML = `Imdb Rating: ` + data.imdbRating
+        boxOffice.innerHTML = `Box office earnings: ` + data.BoxOffice
+        year.innerHTML = `Year of release: ` + data.Year
+        
+        
+        if (data.Title == "undefined") {
+            document.getElementById("info").style.display = "none"
+        }
+    })
+
+    refreshRec()
+    images.forEach(image => {
+        image.addEventListener("click", function() {
+            inputSearch.value = image.alt
+        })
+    })
+}
+
+
+
+document.getElementById("inputSearch").addEventListener("keypress", function(event){
+    if (event.key == "Enter") {
+        document.getElementById("inputBtn").click()
+    }
+})
